c402b151432d364643d3414e2a70d9b3e5a024ea
xurb <xurongbiao@hmtim.com>
2022-05-09 23:22:32 +0800

    过滤器设置中文utf-8

--- src/main/java/com/mashibing/MyFilter.java
+++ src/main/java/com/mashibing/MyFilter.java
@@ -8,7 +8,11 @@ public class MyFilter implements Filter {
 
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("过滤器开始执行");
-        filterChain.doFilter(servletRequest,servletResponse);
 
+        System.out.println(this.getClass().getName()+"-------start");
+        servletRequest.setCharacterEncoding("utf-8");
+        servletResponse.setCharacterEncoding("utf-8");
+        filterChain.doFilter(servletRequest,servletResponse);
+        System.out.println(this.getClass().getName()+"-------stop");
     }
 }
--- src/main/java/com/mashibing/controller/UserController.java
+++ src/main/java/com/mashibing/controller/UserController.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+
 /**
  * localhost:8080/save
  * localhost:8080/update?id=1
@@ -49,5 +53,16 @@ public class UserController {
         System.out.println(this.getClass().getName());
         userDao.query();
         return "success";
+    }
+
+    @RequestMapping("/hello")
+    public void hello(HttpServletRequest request, HttpServletResponse response){
+        try {
+            request.setCharacterEncoding("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        response.setCharacterEncoding("utf-8");
+
     }
 }
